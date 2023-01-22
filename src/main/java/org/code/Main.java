528a246fc89c643dfded6b4e043d0575ed884776
Danyil Bratus <bratusdana@gmail.com>
2023-01-22 08:08:22 -0400

    unit commit

--- src/main/java/org/code/Main.java
+++ src/main/java/org/code/Main.java
@@ -6,14 +6,17 @@ public class Main {
 
         System.out.println("Choose the time in the day (example: '9' or '24')");
         Scanner time = new Scanner(System.in);
-        if (time.nextInt() > 3 && time.nextInt() < 13) {
-            System.out.println("It's a morning!");
-        } else if (time.nextInt() > 12 && time.nextInt() < 18) {
-            System.out.println("It's a day!");
-        } else if ((time.nextInt() > 0 && time.nextInt() < 4) || (time.nextInt() > 18 && time.nextInt() < 25)) {
-            System.out.println("It's a night!");
-        } else {
-        System.out.println("Wrong time!");
+        if (time.hasNextInt()) {
+            int number = time.nextInt();
+            if (number > 3 && number < 13) {
+                System.out.println("It's a morning!");
+            } else if (number > 12 && number < 18) {
+                System.out.println("It's a day!");
+            } else if ((number > 0 && number < 4) || (number > 18 && number < 25)) {
+                System.out.println("It's a night!");
+            } else {
+                System.out.println("Wrong time!");
+            }
         }
     }
 }
